378d6f1bd6d63be32994c41137aa53586e0b9dc8
jlin231 <jonlin981@gmail.com>
2022-12-20 11:14:26 -0800

    added options to migrations

--- backend/db/migrations/20221219231410-create-event.js
+++ backend/db/migrations/20221219231410-create-event.js
@@ -1,5 +1,11 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Events', {
@@ -63,7 +69,7 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
       }
-    });
+    },options);
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Events');
