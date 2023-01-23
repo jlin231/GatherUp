55b2c550989b0bc4dcc3cc278ba9fe590b790b75
jlin231 <jonlin981@gmail.com>
2023-01-23 01:31:40 -0800

    added css for group details

--- frontend/src/components/Group/ShowAllGroups/GroupCard.js
+++ frontend/src/components/Group/ShowAllGroups/GroupCard.js
@@ -15,16 +15,21 @@ function GroupCardComponent({ group }) {
     if (group.private) privacy = 'Private';
     else privacy = 'Public';
 
+    let name= `${group.city}, ${group.state}`;
+    name = name.toUpperCase()
+
     return (
-        <div className="group-card-OuterDiv" onClick={()=>history.push(`/group/${group.id}/about`)}>
+        <div className="group-card-OuterDiv" onClick={() => history.push(`/group/${group.id}/about`)}>
             <div className="group-card-left-div">
-                <img className='previewImg' src={group.previewImage} alt="Not Found"  />
+                <img className='group-cardpreviewImg' src={group.previewImage} alt="Not Found" />
             </div>
             <div className="group-card-right-div">
-                <div className='groupName'>{group.name}</div>
-                <div className='groupCity'>{group.city}, {group.state}</div>
-                <div className='groupAbout'>{group.about}</div>
-                <div>{group.numMembers} members . {privacy}</div>
+                <div className='group-card-right-div-inner'>
+                    <div className='group-card-groupName'>{group.name}</div>
+                    <div className='group-card-groupCity'>{name}</div>
+                    <div className='group-card-groupAbout'>{group.about}</div>
+                </div>
+                <div className='group-card-groupMembers'>{group.numMembers} members . {privacy}</div>
             </div>
         </div>
     );
